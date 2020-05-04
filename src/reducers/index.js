083ec0715dbec70c55c3d619655ab4aeb6645249
sachin.devkar <sachin.devkar@sts.in>
2020-05-04 19:55:27 +0530

    song listing react redux example

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "song1", duration: "4.30" },
+    { title: "song3", duration: "3.30" },
+    { title: "song4", duration: "5.30" },
+    { title: "song5", duration: "8.30" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return null;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
